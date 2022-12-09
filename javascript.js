48ee88f4792fa8fecec44c16fae9193b88353200
trainboy <unanimoususer99@gmail.com>
2022-12-08 22:34:21 -0800

    add cpu choice function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,8 @@
-let
+let selection = ['rock', 'paper', 'scissors']
+let computer = getComputerChoice()
+
+function getComputerChoice() {
+    let comCho = Math.floor(Math.random() * selection.length)
+    return selection[comCho]
+}
+console.log(getComputerChoice())
