147253dbb249c04604cb6799913552ce13edf6f4
trainboy <unanimoususer99@gmail.com>
2022-12-09 00:47:55 -0800

    define logic in function

--- javascript.js
+++ javascript.js
@@ -1,8 +1,36 @@
 let selection = ['rock', 'paper', 'scissors']
 let computer = getComputerChoice()
+let playerChoice = 'paper'
+let player = playerChoice
+
+console.log('ME: ' + player)
+console.log('CPU: ' + computer)
 
 function getComputerChoice() {
     let comCho = Math.floor(Math.random() * selection.length)
     return selection[comCho]
 }
-console.log(getComputerChoice())
+
+
+function playRound() {
+    if (player === 'rock' && computer === 'paper') {
+        return 'you lose, paper covers rock'
+    } else if (player === 'paper' && computer === 'scissors') {
+        return 'you lose, scissors cuts paper'
+    } else if ( player === 'scissors' && computer === 'rock') {
+        return 'you lose, rock crushes scissors'
+    } else if (player === 'rock' && computer === 'scissors') {
+        return 'YOU WIN! rock crushes scissors'
+    } else if (player === 'paper' && computer === 'rock') {
+        return 'YOU WIN! paper covers rock'
+    } else if (player === 'scissors' && computer === 'paper') {
+        return 'YOU WIN! scissors cuts paper'
+    } else if (player === 'rock' && computer === 'rock') {
+        return 'DRAW: rock & rock'
+    } else if (player === 'paper' && computer === 'paper') {
+        return 'DRAW: paper & paper'
+    } else if (player === 'scissors' && computer === 'scissors') {
+        return 'DRAW: scissors & scissors'
+    }
+}
+console.log(playRound())
