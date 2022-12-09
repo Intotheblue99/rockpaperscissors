c2a9e32a6237bd59d34eef750d1a675e63846f29
trainboy <unanimoususer99@gmail.com>
2022-12-09 04:14:37 -0800

    add prompt function

--- javascript.js
+++ javascript.js
@@ -1,11 +1,17 @@
 let selection = ['rock', 'paper', 'scissors']
 let computer = getComputerChoice()
-let player = getPlayerChoice()
+let player = prompt()
+let playerScore = 0
+let cpuScore = 0
+let cpuWin = false
+let playerWin = false
 //let playerChoice = 'paper'
 //let player = playerChoice
 
 console.log('ME: ' + player)
 console.log('CPU: ' + computer)
+
+
 
 function getComputerChoice() {
     let comCho = Math.floor(Math.random() * selection.length)
@@ -13,25 +19,37 @@ function getComputerChoice() {
 }
 
 // temporary//
-function getPlayerChoice() {
-    let plaCho = Math.floor(Math.random() * selection.length)
-    return selection[plaCho]
-}
+// function getPlayerChoice() {
+//     let plaCho = Math.floor(Math.random() * selection.length)
+//     return selection[plaCho]
+// }
 
 
-function playRound() {
+function playRound(player, computer) {
     if (player === 'rock' && computer === 'paper') {
-        return 'you lose, paper covers rock'
+        cpuWin = true
+        return 'you lose, paper covers rock';
+        
     } else if (player === 'paper' && computer === 'scissors') {
-        return 'you lose, scissors cuts paper'
+        cpuWin = true
+        return 'you lose, scissors cuts paper';
+        //cpuWin = true
     } else if ( player === 'scissors' && computer === 'rock') {
-        return 'you lose, rock crushes scissors'
+        cpuWin = true
+        return 'you lose, rock crushes scissors';
+        //cpuWin = true
     } else if (player === 'rock' && computer === 'scissors') {
-        return 'YOU WIN! rock crushes scissors'
+        playerWin = true
+        return 'YOU WIN! rock crushes scissors';
+        //playerWin = true
     } else if (player === 'paper' && computer === 'rock') {
-        return 'YOU WIN! paper covers rock'
+        playerWin = true
+        return 'YOU WIN! paper covers rock';
+        //playerWin = true
     } else if (player === 'scissors' && computer === 'paper') {
-        return 'YOU WIN! scissors cuts paper'
+        playerWin = true
+        return 'YOU WIN! scissors cuts paper';
+        //playerWin = true
     } else if (player === 'rock' && computer === 'rock') {
         return 'DRAW: rock & rock'
     } else if (player === 'paper' && computer === 'paper') {
@@ -40,4 +58,26 @@ function playRound() {
         return 'DRAW: scissors & scissors'
     }
 }
-console.log(playRound())
+
+console.log(playRound(player, computer))
+
+function game() {
+    playRound()
+    for (i = 0; i < 5; i++) {
+        playRound(i)
+        prompt()
+        if (playerWin === true) {
+            console.log('round ' + [i] + ': WIN')
+        } else {
+            console.log('round ' + [i] + ': LOSS')
+        }
+        
+   }
+}
+game()
+
+//console.log('CPU SCORE: ' + cpuScore)
+//console.log('PLAYER SCORE: ' + playerScore)
+
+//if (cpuWin === true) {
+//   cpuWin += 1 
